0301a85bbcff9130d9441d208633eb62dcb8aa5e
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-07-04 13:41:16 +0700

    ubah migrate on delete cascade

--- database/migrations/2020_07_03_002459_create_answers_table.php
+++ database/migrations/2020_07_03_002459_create_answers_table.php
@@ -19,7 +19,7 @@ class CreateAnswersTable extends Migration
             $table->unsignedBigInteger('question_id');
             //$table->unsignedBigInteger('user_id');
             $table->timestamps();
-            $table->foreign('question_id')->references('id')->on('questions');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             //$table->foreign('user_id')->references('id')->on('users');
         });
     }
